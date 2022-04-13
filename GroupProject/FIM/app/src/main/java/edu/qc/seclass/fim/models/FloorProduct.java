e849851bcd69656da65f0968cf83cd506763fbd1
alereyes18 <33614808+alereyes18@users.noreply.github.com>
2022-04-13 12:59:36 -0400

    Implemented the floor class

--- GroupProject/FIM/app/src/main/java/edu/qc/seclass/fim/models/FloorProduct.java
+++ GroupProject/FIM/app/src/main/java/edu/qc/seclass/fim/models/FloorProduct.java
@@ -2,15 +2,86 @@ package edu.qc.seclass.fim.models;
 
 public class FloorProduct {
 
-    private String category;
-    private String type;
+    Integer floorID;
+    String floorCategory;
+    String floorType;
+    String floorSpecies;
+    String floorColor;
+    Double price;
+    Double size;
+    Integer quantity;
 
-
-    public String getCategory() {
-        return category;
+    //Constructor
+    public FloorProduct(Integer id, String category, String type, String species, String color){
+        this.floorID = id;
+        this.floorCategory = category;
+        this.floorType = type;
+        this.floorSpecies = species;
+        this.floorColor = color;
     }
 
-    public void setCategory(String category) {
-        this.category = category;
+    //Getters and setters
+    public Integer getFloorID() {
+        return floorID;
+    }
+
+    public void setFloorID(Integer floorID) {
+        this.floorID = floorID;
+    }
+
+    public String getFloorCategory() {
+        return floorCategory;
+    }
+
+    public void setFloorCategory(String floorCategory) {
+        this.floorCategory = floorCategory;
+    }
+
+    public String getFloorType() {
+        return floorType;
+    }
+
+    public void setFloorType(String floorType) {
+        this.floorType = floorType;
+    }
+
+    public String getFloorSpecies() {
+        return floorSpecies;
+    }
+
+    public void setFloorSpecies(String floorSpecies) {
+        this.floorSpecies = floorSpecies;
+    }
+
+    public String getFloorColor() {
+        return floorColor;
+    }
+
+    public void setFloorColor(String floorColor) {
+        this.floorColor = floorColor;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Double getSize() {
+        return size;
+    }
+
+    public void setSize(Double size) {
+        this.size = size;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
     }
 }
